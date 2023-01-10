4a91c8fa5fe2b4a0d2730a02ccc1ffc9cc81d697
Hexona <xuanjiap@lolicon.ac.cn>
2023-01-10 06:00:30 +0000

    feat: supporting both connection mode

--- src/configs/template-auth.ts
+++ src/configs/template-auth.ts
@@ -1,4 +1,5 @@
 export default {
+    mode: 'websocket',
     khlport: 6000,
     khlkey: 'encrypt key here',
     khltoken: 'token here',
--- src/init/client.ts
+++ src/init/client.ts
@@ -1,8 +1,9 @@
 import { KBotify } from 'kbotify';
 import auth from '../configs/auth';
 
+let mode = <'websocket' | 'webhook'>auth.mode || 'websocket';
 export const bot = new KBotify({
-    mode: 'websocket', //确保和开黑啦应用的后台设置一样。如果使用webhook，请详细阅读开发者手册关于"?compress=0"的部分。
+    mode: mode, //确保和开黑啦应用的后台设置一样。如果使用webhook，请详细阅读开发者手册关于"?compress=0"的部分。
     token: auth.khltoken,
     port: auth.khlport,
     verifyToken: auth.khlverify,
